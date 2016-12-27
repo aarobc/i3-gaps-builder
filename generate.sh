d75a989a90d77608a295c9dd1f6e02c5126ea2af
ac <aaron.christiansen@clearlink.com>
2016-12-27 13:20:55 -0700

    updated to acomodate new build process

--- generate.sh
+++ generate.sh
@@ -3,7 +3,12 @@ git clone -b gaps-next https://github.com/Airblader/i3.git /opt/i3-gaps-next
 # for subsequent runs
 cd /opt/i3-gaps-next && git pull
 cd /opt/i3-gaps-next &&
-  checkinstall --install=no --pkgversion 1 --pakdir /opt/deb --nodoc -y\
+  autoreconf --force --install \
+  && rm -rf build/ \
+  && mkdir -p build && cd build/ \
+  && ../configure --prefix=/usr --sysconfdir=/etc \
+  && make \
+  && checkinstall --install=no --pkgversion 1 --pakdir /opt/deb --nodoc -y\
   && git clean -f
 
 cd /tmp/xcb-util-xrm &&\
